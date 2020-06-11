dd5922377715c0757225c980f3d6ee417fe8cd1b
Riad Benguella <benguella@gmail.com>
2020-06-11 14:48:28 +0100

    Show one inserter at a time

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -124,7 +124,7 @@ class EditorProvider extends Component {
 				'__experimentalGlobalStylesUserEntityId',
 				'__experimentalGlobalStylesBase',
 				'__experimentalPreferredStyleVariations',
-				'__experimentalOnExpandInserter',
+				'__experimentalSetIsInserterOpened',
 				'alignWide',
 				'allowedBlockTypes',
 				'availableLegacyWidgets',
